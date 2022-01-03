fc006de453b192d8c00e97bba8332bd5e5cfb1a8
Mike Maliszewski <mike@oceansidelabs.io>
2022-01-03 16:52:48 -0600

    created index.html and added routes for serving portfolio and version information

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import path from 'path'
+import childProcess from 'child_process'
 import { Configuration } from './Configuration'
 
 class App {
@@ -6,10 +8,27 @@ class App {
 
 	configure() {
 		this.express.disable('x-powered-by')
+		this.express.get('/', this.sendPortfolio)
+		this.express.get('/version', this.sendVersionInfo)
 		this.express.listen(Configuration.shared.port, () => {
 			console.log(
 				`Server now listening on port ${Configuration.shared.port}`
 			)
+		})
+	}
+
+	private sendPortfolio(req: Request, res: Response) {
+		res.sendFile(path.join(__dirname, '../', 'static/index.html'))
+	}
+
+	private sendVersionInfo(req: Request, res: Response) {
+		// get commit hash and return it
+		childProcess.exec('git rev-parse HEAD', (error, commitHash) => {
+			const response = {
+				commit: commitHash.trim(),
+				date: new Date(),
+			}
+			res.json(response)
 		})
 	}
 }
